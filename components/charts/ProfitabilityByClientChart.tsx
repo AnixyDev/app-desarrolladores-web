e4ef656e188ea5f050d636cd55b8014781c9c1aa
Anixy {Dev} <anixydev@gmail.com>
2026-01-26 20:29:26 +0100

    Update ProfitabilityByClientChart.tsx

--- components/charts/ProfitabilityByClientChart.tsx
+++ components/charts/ProfitabilityByClientChart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import { formatCurrency } from '@/lib/utils.ts';
+import { formatCurrency } from '@/lib/utils';
 
 interface ProfitabilityData {
   name: string;
