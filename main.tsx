613397c95db5c4c6b371caad4d86fe61347dd174
Anixy {Dev} <anixydev@gmail.com>
2026-01-26 22:44:22 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
-import { useAppStore } from '@/hooks/useAppStore';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import './index.css';
+
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
