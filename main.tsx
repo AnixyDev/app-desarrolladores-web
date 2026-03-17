e7fe2cf605a53377913240a10b6500e3d0bb6206
Anixy {Dev} <anixydev@gmail.com>
2026-03-17 21:00:13 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <--- ESTO FALTABA
+import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./index.css";
 
+// ID de cliente de Google Cloud Console
 const GOOGLE_CLIENT_ID = "457438236235-n2s8q6nvcjm32u0o3ut2lksd8po8gfqf.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <--- ENVOLVEMOS LA APP PARA QUE EL ROUTER FUNCIONE */}
+    {/* El BrowserRouter debe envolverlo TODO para que funcionen las rutas */}
+    <BrowserRouter>
       <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
