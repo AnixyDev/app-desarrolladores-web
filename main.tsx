301feaad06255df9d7fc477902030626d6d228de
Anixy {Dev} <anixydev@gmail.com>
2026-03-22 15:26:05 +0100

    Refactor GoogleOAuthProvider placement in main.tsx
    Moved GoogleOAuthProvider outside of StrictMode to prevent multiple initializations of the Google SDK during development.

--- main.tsx
+++ main.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import App from "./App";
-import "./index.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+import './index.css';
 
-// ID de cliente de Google Cloud Console
-const GOOGLE_CLIENT_ID = "457438236235-n2s8q6nvcjm32u0o3ut2lksd8po8gfqf.apps.googleusercontent.com";
+// FIX: GoogleOAuthProvider fuera de StrictMode para evitar la doble
+// inicialización de google.accounts.id.initialize() que causa el aviso
+// "[GSI_LOGGER]: google.accounts.id.initialize() is called multiple times"
+// StrictMode monta efectos dos veces en desarrollo, lo que dispara dos
+// inicializaciones del SDK de Google y comportamiento impredecible.
+
+const GOOGLE_CLIENT_ID = '457438236235-n2s8q6nvcjm32u0o3ut2lksd8po8gfqf.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* El BrowserRouter debe envolverlo TODO para que funcionen las rutas */}
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <React.StrictMode>
+      <BrowserRouter>
         <App />
-      </GoogleOAuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
