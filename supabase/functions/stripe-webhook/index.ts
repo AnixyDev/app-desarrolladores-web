d69944a582fb409742cc5528757e04b75d61235c
Anixy {Dev} <anixydev@gmail.com>
2026-06-02 19:55:56 +0200

    Ensure endpointSecret is non-null and add logs
    Add non-null assertion for endpointSecret and debug logs.

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -9,7 +9,7 @@ const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') as string, {
   typescript: true,
 })
 
-const endpointSecret = Deno.env.get('STRIPE_WEBHOOK_SECRET');
+const endpointSecret = Deno.env.get('STRIPE_WEBHOOK_SECRET')!; // El signo ! le dice a TS que confías en que no es null
 
 // AÑADE ESTO PARA DEBUGUEAR (mira los logs después de hacer el despliegue)
 console.log("¿Existe el secreto?", !!endpointSecret);
